a3cb106125e14a051bf8b73fdee2e102a106b2c4
Flemming Behrend <flemmingjaeger@gmail.com>
2015-10-18 19:25:34 +0200

    Initial commit

---
+++ src/main/java/dk/topdanmark/auction/test/boundary/TestResource.java
@@ -0,0 +1,18 @@
+package dk.topdanmark.auction.test.boundary;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/tester")
+public class TestResource {
+
+    @GET
+    public Response getAll() {
+
+        return Response.ok("tester tester test").build();
+    }
+
+
+
+}
